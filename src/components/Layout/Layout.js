f4e2734dc03c2fa6ea3076588c9acebd6b44f26d
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-06-19 22:12:37 -0300

    new: create layout component and its style css

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Header from "../Global/Header/Header";
+import Navbar from "../Global/Navbar/Navbar";
+import Footer from "../Global/Footer/Footer";
+
+import "./Layout.css";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
