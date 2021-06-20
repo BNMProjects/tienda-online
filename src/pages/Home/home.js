639addf7524d3059e81d3aefa0a10072b1f09e55
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-06-19 22:11:34 -0300

    new: create home page

---
+++ src/pages/Home/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../../components/Layout/Layout";
+
+const Home = () => {
+  return (
+    <>
+      <Layout>
+        <h1>Esta es la pagina principal</h1>
+      </Layout>
+    </>
+  );
+};
+
+export default Home;
