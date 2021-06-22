0535dbedb040e2c3c06bfd274367df0404be539d
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-06-22 15:45:19 -0300

    fix: change route to components in import

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Header from "../Global/Header/index";
-import Footer from "../Global/Footer/index";
-import Navbar from "../Global/Navbar/index";
+import Header from "../Header/index";
+import Footer from "../Footer/index";
+import Navbar from "../Navbar/index";
 
 const Layout = ({ children }) => {
   return (
