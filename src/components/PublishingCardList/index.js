1956c36b39af3f8b0a53f2e317a26d61650dfb57
Leandro Murgo <leandrouni01@gmail.com>
2021-07-02 12:32:38 -0300

    fix(components publishingCardList) add export default and key value

--- src/components/PublishingCardList/index.js
+++ src/components/PublishingCardList/index.js
@@ -3,12 +3,14 @@ import PublishingCard from '../../components/PublishingCard';
 
 import * as S from './styles';
 
-export const PublishingCardList = ({publishings}) => {
+const PublishingCardList = ({publishings}) => {
  return (
   <S.PublishingCardList >
     {publishings.map(p => {
-      return <PublishingCard publication={p} />
+      return <PublishingCard key={p.id} publication={p} />
     })}
   </S.PublishingCardList>
  )
 }
+
+export default PublishingCardList;
